7407440d9c0092a6adb5c050c7a889390dea954f
Prashanth <reddyprashanth251@gmail.com>
2024-10-30 14:45:26 -0400

    exception level update

--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/exception/CustomizedResponseEntityExceptionHandler.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/exception/CustomizedResponseEntityExceptionHandler.java
@@ -41,7 +41,7 @@ public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExce
     protected ResponseEntity<Object> handleMethodArgumentNotValid
             (MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
         ErrorDetails errorDetails = new ErrorDetails(LocalDateTime.now(),
-                "Total Errors:" + ex.getErrorCount() + " First Error: " + ex.getFieldError().getDefaultMessage() ,request.getDescription(false));
+                "Total Errors:" + ex.getErrorCount() + " First Error: " + ex.getFieldError().getDefaultMessage(), request.getDescription(false));
 
 
         return new ResponseEntity(errorDetails, HttpStatus.BAD_REQUEST);
